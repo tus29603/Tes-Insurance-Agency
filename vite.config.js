395e1e3b420cf9c102e203455f50df9ebcff7677
Tesfaldet haileab <tus29603@temple.edu>
2025-09-12 10:56:07 -0400

    update quote form and routes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// replace with your repo name:
+const repoName = 'Tes-Insurance-Agency'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`, // needed so assets resolve on GH Pages
 })
